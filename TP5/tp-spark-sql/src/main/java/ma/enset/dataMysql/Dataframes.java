75b6739c69ce44dda3c7a488fb2f1aa6fec6e1fc
Ayoub ETOULLALI <ayoub.etoullali2002@example.com>
2023-04-08 17:54:23 +0000

    update

--- TP5/tp-spark-sql/src/main/java/ma/enset/dataMysql/Dataframes.java
+++ TP5/tp-spark-sql/src/main/java/ma/enset/dataMysql/Dataframes.java
@@ -3,8 +3,11 @@ package ma.enset.dataMysql;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.RelationalGroupedDataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import static org.apache.spark.sql.functions.col;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,7 +19,12 @@ public class Dataframes {
     public static void main(String[] args) {
 
         Logger.getLogger("org").setLevel(Level.OFF);
-        SparkSession ss=SparkSession.builder().master("local[*]").appName("Traitement de données stockées dans Mysql [Dataset,Dataframe]").getOrCreate();
+
+        SparkSession ss=SparkSession
+                .builder()
+                .master("local[*]")
+                .appName("Traitement de données stockées dans Mysql [Dataset,Dataframe]")
+                .getOrCreate();
 
         Map<String,String> options=new HashMap<>();
         options.put("driver","com.mysql.cj.jdbc.Driver");
@@ -30,18 +38,22 @@ public class Dataframes {
                 .option("dbtable","CONSULTATIONS")
                 .load();
         df1.select("DATE_CONSULTATION").groupBy("DATE_CONSULTATION").count().show();
+//        df1.select("DATE_CONSULTATION").as("Date de consultaion").groupBy("DATE_CONSULTATION").count().as("Nombre").show();
 
         System.out.println("\n\n----------------Afficher le nombre de consultation par médecin----------------\n");
-        Dataset<Row> df2_1 = ss.read().format("jdbc").options(options).option("dbtable","CONSULTATIONS").load();
-        Dataset<Row> df2_2 = ss.read().format("jdbc").options(options).option("dbtable","MEDECINS").load();
-        Dataset<Row> f2_merge=df2_1.join(df2_2);
-        //f2_merge.show();
-        f2_merge.select("NOM","PRENOM").groupBy("NOM","PRENOM").count().as("NOMBRE DE CONSULTATION").show();
+        Dataset<Row> consultations = ss.read().format("jdbc").options(options).option("dbtable","CONSULTATIONS").load();
+        Dataset<Row> medecins = ss.read().format("jdbc").options(options).option("dbtable","MEDECINS").load();
+        consultations
+                .join(medecins, consultations.col("ID_MEDECIN").equalTo(medecins.col("ID")))
+                .groupBy(medecins.col("NOM"), medecins.col("PRENOM"))
+                .count().as("NB_CONSULTATIONS")
+                .show();
 
         System.out.println("\n\n----------------Afficher pour chaque médecin, le nombre de patients qu’il a assisté----------------\n");
-        Dataset<Row> df3_1 = ss.read().format("jdbc").options(options).option("dbtable","MEDECINS").load();
-        Dataset<Row> df3_2 = ss.read().format("jdbc").options(options).option("dbtable","CONSULTATIONS").load();
-        Dataset<Row> f3_merge=df3_1.join(df3_2);
-        f2_merge.select("NOM","PRENOM","ID_PATIENT").groupBy("NOM","PRENOM","ID_PATIENT").agg(countDistinct("ID_PATIENT").as("NOMBRE DE PATIENT")).show();
+        consultations
+                .join(medecins, consultations.col("ID_MEDECIN").equalTo(medecins.col("ID")))
+                .groupBy(medecins.col("NOM"), medecins.col("PRENOM"))
+                .agg(countDistinct(consultations.col("ID_PATIENT")).as("NB_PATIENTS")).show();
+
     }
 }
--- TP5/tp-spark-sql/src/main/java/ma/enset/dataMysql/QuerySql.java
+++ TP5/tp-spark-sql/src/main/java/ma/enset/dataMysql/QuerySql.java
@@ -5,6 +5,7 @@ import org.apache.log4j.Logger;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import static org.apache.spark.sql.functions.col;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,7 +15,7 @@ public class QuerySql {
     public static void main(String[] args) {
 
         Logger.getLogger("org").setLevel(Level.OFF);
-        SparkSession ss=SparkSession.builder().master("local[*]").appName("Traitement de données stockées dans Mysql [Dataset,Dataframe]").getOrCreate();
+        SparkSession ss=SparkSession.builder().master("local[*]").appName("Traitement de données stockées dans Mysql [Query SQL]").getOrCreate();
 
         Map<String,String> options=new HashMap<>();
         options.put("driver","com.mysql.cj.jdbc.Driver");
@@ -26,21 +27,46 @@ public class QuerySql {
         Dataset<Row> df1 = ss.read().format("jdbc")
                 .options(options)
                 //.option("dbtable","CONSULTATIONS")
-                .option("query","select DATE_CONSULTATION , count( from CONSULTATIONS group by DATE_CONSULTATION)")
+//                .option("query","select DATE_CONSULTATION , count( from CONSULTATIONS group by DATE_CONSULTATION)")
+                .option("query","SELECT DATE_CONSULTATION, COUNT(*) AS NB_CONSULTATIONS\n" +
+                        "FROM CONSULTATIONS\n" +
+                        "GROUP BY DATE_CONSULTATION")
                 .load();
         df1.show();
 
         System.out.println("\n\n----------------Afficher le nombre de consultation par médecin----------------\n");
+        Dataset<Row> df2 = ss.read().format("jdbc")
+                .options(options)
+                /*
+                .option("query", "select medecins.NOM, medecins.PRENOM, COUNT(medecins.ID) AS nombre_de_consultations " +
+                        "from consultations " +
+                        "join medecins on medecins.ID=consultations.ID_MEDECIN " +
+                        "group by consultations.ID_MEDECIN")
+                 */
+                .option("query","SELECT M.NOM, M.PRENOM, COUNT(*) AS NB_CONSULTATIONS\n" +
+                        "FROM CONSULTATIONS C\n" +
+                        "JOIN MEDECINS M ON C.ID_MEDECIN = M.ID\n" +
+                        "GROUP BY M.NOM, M.PRENOM")
+                .load();
+        df2.show();
+
 
         System.out.println("\n\n----------------Afficher pour chaque médecin, le nombre de patients qu’il a assisté----------------\n");
-
-
-        /*Dataset<Row> dfEmp = ss.read().format("jdbc")
+        Dataset<Row> df3 = ss.read().format("jdbc")
                 .options(options)
-                //.option("dbtable", "EMPLOYES")
-                .option("query","select ID,NOM,PRENOM from PATIENTS")
+                /* [N.B] pas complet
+                .option("query", "SELECT medecins.NOM, medecins.PRENOM, COUNT(patients.ID) AS nombre_de_patients\n" +
+                        "FROM consultations\n" +
+                        "JOIN medecins ON medecins.ID=consultations.ID_MEDECIN\n" +
+                        "JOIN patients ON patients.ID=consultations.ID_PATIENT\n" +
+                        "GROUP BY consultations.ID_MEDECIN, consultations.ID_PATIENT")
+                 */
+                .option("query", "SELECT M.NOM, M.PRENOM, COUNT(DISTINCT C.ID_PATIENT) AS NB_PATIENTS\n" +
+                        "FROM CONSULTATIONS C\n" +
+                        "JOIN MEDECINS M ON C.ID_MEDECIN = M.ID\n" +
+                        "GROUP BY M.NOM, M.PRENOM")
                 .load();
-        dfEmp.printSchema();
-        dfEmp.show();*/
+        df3.show();
+
     }
 }
